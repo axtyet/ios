d7a6c7d43494da6bc5f7b69b0005cc16d7bd7534
axtyet <144403008+axtyet@users.noreply.github.com>
2023-10-28 21:04:23 +0800

    Update tiimo.js

--- ReWrite_Chxm1023/Revenuecat/tiimo.js
+++ ReWrite_Chxm1023/Revenuecat/tiimo.js
@@ -9,9 +9,8 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Revenuecat/tiimo.js
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/Revenuecat/tiimo.js
-
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/axtyet/ios/main/ReWrite_Chxm1023/Revenuecat/tiimo.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/axtyet/ios/main/ReWrite_Chxm1023/Revenuecat/tiimo.js
 [mitm]
 hostname = api.revenuecat.com
 
